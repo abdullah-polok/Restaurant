0eed402c91d6f0f068c188cddb90acc1cde73c32
Abdullah's PC <abdullahalrahman23@gmail.com>
2023-11-22 21:51:50 +0300

    send image to imgBB using axios and imgBB api

--- src/pages/Dashboard/AddItems/AddItems.jsx
+++ src/pages/Dashboard/AddItems/AddItems.jsx
@@ -1,10 +1,24 @@
 import SectionTitle from "../../../components/SectionTitle/SectionTitle";
 import { useForm } from "react-hook-form"
 import { FaUtensils } from "react-icons/fa";
+import useAxiosPublic from '../../../hooks/useAxiosPublic'
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+
 const AddItems = () => {
     const { register, handleSubmit } = useForm()
-    const onSubmit = (data) => {
+    const axiosPublic = useAxiosPublic()
+    const onSubmit = async (data) => {
         console.log(data)
+
+        ////upload to the imgbb and then get an url
+        const imageFile = { image: data.image[0] }
+        const res = await axiosPublic.post(image_hosting_api, imageFile, {
+            headers: {
+                'content-type': 'multipart/form-data'
+            }
+        })
+        console.log(res)
     }
     return (
         <div>
@@ -25,8 +39,8 @@ const AddItems = () => {
                             <label className="label">
                                 <span className="label-text-alt">Category*</span>
                             </label>
-                            <select {...register("category", { required: true })} className="mb-5 select select-bordered w-full">
-                                <option disabled selected>Select a category</option>
+                            <select defaultValue={"Default"} {...register("category", { required: true })} className="mb-5 select select-bordered w-full">
+                                <option value={"default"}>Select a category</option>
                                 <option value={"salad"}>Salad</option>
                                 <option value={"pizza"}>Pizza</option>
                                 <option value={"soup"}>Soup</option>
